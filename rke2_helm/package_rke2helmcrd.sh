5de578c924ea63964501d9d1c54ed8ce0645383f
Brian Durden <brian.durden@ranchergovernment.com>
2024-11-18 14:15:13 -0500

    fixed

--- rke2_helm/package_rke2helmcrd.sh
+++ rke2_helm/package_rke2helmcrd.sh
@@ -1,18 +1,15 @@
 #!/bin/bash
 
-pushd charts/rke2
-    tar czvf /tmp/rke2.tar.gz . 
-popd
+helm package charts/rke2
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS
-    export CHART_B64=$(cat /tmp/rke2.tar.gz | base64)
+    export CHART_B64=$(cat rke2-cluster*.tgz | base64)
 else
     # Linux
-    export CHART_B64=$(cat /tmp/rke2.tar.gz | base64 -w0)
+    export CHART_B64=$(cat rke2-cluster*.tgz | base64 -w0)
 fi
 
-rm /tmp/rke2.tar.gz
 
 yq '.spec.valuesContent = load_str("values.yaml")' rke2_helmcrd_template.yaml | yq '.spec.chartContent = strenv(CHART_B64)' > rke2helmcrd.yaml
 
